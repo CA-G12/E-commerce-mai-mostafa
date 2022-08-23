e8e6cc37e69efea910bd31ff200008c41df64e24
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-08-23 23:29:44 +0300

    connection with database and create router file relates #5

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const compression = require('compression');
-
+const router =require('./router')
 
 const app = express();
+
 app.set('port', process.env.PORT || 3000)
 
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }));
+
 app.use(compression());
+
+app.use(router)
+
 module.exports = app
